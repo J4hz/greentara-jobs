f21f3be569c25d7f6869400792faa2f557b0aa4d
J4hz <mwangijahdai@outlook.com>
2025-10-21 12:41:28 +0300

    Fix Pillow and static files

--- greentara/settings.py
+++ greentara/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
+
 
 # Media files (uploads)
 MEDIA_URL = '/uploads/'
